3a92752a0899b628baa2f774a1e8558febf9f9a5
S4ndndj72 <herokus10@gmail.com>
2024-12-23 22:56:52 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,13 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:3000',
     credentials: true,
   },
 });
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:3000',
     credentials: true,
 }));
 app.use(express.json());

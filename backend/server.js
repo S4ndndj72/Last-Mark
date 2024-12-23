ce8c192d751df9a70544f327ee813e712cfd8151
S4ndndj72 <herokus10@gmail.com>
2024-12-23 22:55:07 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const http = require('http');
-const { Server, io } = require('socket.io');
+const { Server } = require('socket.io');
 const jwt = require('jsonwebtoken'); 
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin');
@@ -17,10 +17,15 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 
-var socket = io.connect('http://localhost:8080')
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000'
+    credentials: true,
+  },
+});
 
 app.use(cors({
-    origin: "*",
+    origin: 'http://localhost:3000'
     credentials: true,
 }));
 app.use(express.json());
@@ -187,5 +192,5 @@ app.use('/api/chat', chatRoutes);
 app.get("/:universalURL", (req, res) => {
    res.send("404 URL NOT FOUND..");
 });
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
